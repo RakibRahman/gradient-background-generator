d326135e1b26bd234be2b92f1457fcc80a797c07
RakibRahman <md.rr.talukder@gmail.com>
2021-04-25 07:06:22 +0600

    select,copy,tooptip added

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const btn1 = document.querySelector("#btn1");
+const colorDirection = document.querySelector("#direction");
+const output = document.querySelector("#output");
 
 const linearGradient = () => {
-  document.body.style.background = `linear-gradient(to bottom, ${color1.value}, ${color2.value}`;
+  const dirValue = colorDirection.options[colorDirection.selectedIndex].value;
+  document.body.style.background = `linear-gradient(${dirValue}, ${color1.value}, ${color2.value}`;
+  output.textContent = `background: linear-gradient(${dirValue}, ${color1.value}, ${color2.value});`;
 };
-btn1.onclick = linearGradient;
+
+btn1.addEventListener("click", linearGradient);
+
+output.addEventListener("click", () => {
+  let copyValue = output.innerText;
+  let elem = document.createElement("textarea");
+  document.body.appendChild(elem);
+  elem.value = copyValue;
+  elem.select();
+  elem.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  document.body.removeChild(elem);
+  alert("Copied the text: " + elem.value);
+});
